7920b9da3c76d54e121ad929f32bbb676ed868ed
Safonova Svetlana <Safonova_s@icloud.com>
2020-10-14 17:26:59 +0300

    Initial Commit

---
+++ Task1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task1
+//
+//  Created by Светлана Сафонова on 14.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
